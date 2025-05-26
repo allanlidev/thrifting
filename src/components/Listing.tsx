f4777dcefe8b77cdf8fbfc136bc6520ae95e8756
Emil Calonius <emil.calonius@gmail.com>
2025-05-26 13:48:53 +0300

    feat(home): add price to listing list items

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -4,12 +4,13 @@ import { Large } from '~/src/components/ui/typography'
 import { Skeleton } from '~/src/components/ui/skeleton'
 import { RemoteImage } from '~/src/components/RemoteImage'
 import { Tables } from '~/src/database.types'
+import { Text } from '~/src/components/ui/text'
 
 export function Listing({ item, onPress }: { item: Tables<'products'>; onPress?: () => void }) {
   return (
     <Pressable onPress={onPress} className="active:opacity-50">
-      <View className="mt-4 gap-3">
-        <View className="h-40 w-40 rounded-md bg-muted">
+      <View className="mt-4 gap-2">
+        <View className="h-40 w-40 overflow-hidden rounded-md bg-muted">
           {item.images.length > 0 && (
             <RemoteImage
               bucketId="product-images"
@@ -19,6 +20,9 @@ export function Listing({ item, onPress }: { item: Tables<'products'>; onPress?:
               resizeMode="cover"
             />
           )}
+          <Text className="absolute bottom-0 m-2 rounded-md bg-background/50 px-2 py-1">
+            {item.price}€
+          </Text>
         </View>
         <Large numberOfLines={3} ellipsizeMode="tail" className="w-40">
           {item.title ?? t`No title`}
