27a129f83a060f7178393e2ab1b2edb33db0e226
Allan Li <allan0499@gmail.com>
2025-05-29 22:35:34 +0300

    refactor(login, signup): add form validation & submission state

--- src/app/(app)/login.tsx
+++ src/app/(app)/login.tsx
@@ -8,22 +8,29 @@ import { Text } from '~/src/components/ui/text'
 import { Trans } from '@lingui/react/macro'
 import { t } from '@lingui/core/macro'
 import { H1 } from '~/src/components/ui/typography'
+import { z } from 'zod'
+import { useForm } from '@tanstack/react-form'
+import { ErrorMessage } from '~/src/components/ErrorMessage'
 
 export default function Login() {
   const { logIn } = useAuth()
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  const formSchema = z.object({
+    email: z.string().email({ message: t`Invalid email address` }),
+    password: z.string(),
+  })
 
-  /**
-   * Sign in with email and password.
-   */
-  async function signInWithEmail() {
-    setIsLoading(true)
-    await logIn({ email, password })
-    setIsLoading(false)
-  }
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    validators: { onSubmit: formSchema },
+    onSubmit: async ({ value }) => {
+      const { email, password } = value
+      await logIn({ email, password })
+    },
+  })
 
   return (
     <KeyboardAvoidingView behavior="padding" className="flex-1">
@@ -40,42 +47,63 @@ export default function Login() {
             <Label nativeID="emailInput">
               <Trans>Email</Trans>
             </Label>
-            <Input
-              onChangeText={(text) => setEmail(text)}
-              value={email}
-              placeholder={t`email@address.com`}
-              autoCapitalize="none"
-              keyboardType="email-address"
-              autoComplete="email"
-              autoCorrect={false}
-              aria-labelledby="emailInput"
-              aria-errormessage="emailError"
+            <form.Field
+              name="email"
+              children={(field) => (
+                <>
+                  <Input
+                    onChangeText={field.handleChange}
+                    value={field.state.value}
+                    placeholder={t`email@address.com`}
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    autoComplete="email"
+                    autoCorrect={false}
+                    aria-labelledby="emailInput"
+                    aria-errormessage="emailError"
+                  />
+                  {field.state.meta.errors.length > 0 && (
+                    <ErrorMessage
+                      msg={field.state.meta.errors.map((error) => error?.message).join(', ')}
+                    />
+                  )}
+                </>
+              )}
             />
           </View>
           <View className="w-full gap-2">
             <Label nativeID="passwordInput">
               <Trans>Password</Trans>
             </Label>
-            <Input
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              placeholder={t`Password`}
-              autoCapitalize="none"
-              autoComplete="password"
-              autoCorrect={false}
-              aria-labelledby="passwordInput"
-              aria-errormessage="passwordError"
-              secureTextEntry
+            <form.Field
+              name="password"
+              children={(field) => (
+                <Input
+                  value={field.state.value}
+                  onChangeText={field.handleChange}
+                  placeholder={t`Password`}
+                  autoCapitalize="none"
+                  autoComplete="password"
+                  autoCorrect={false}
+                  aria-labelledby="passwordInput"
+                  aria-errormessage="passwordError"
+                  secureTextEntry
+                />
+              )}
             />
           </View>
-          <Button
-            disabled={isLoading}
-            onPress={() => signInWithEmail()}
-            className="flex-row gap-2 self-stretch"
-          >
-            {isLoading && <ActivityIndicator />}
-            <Text>{isLoading ? <Trans>Signing in...</Trans> : <Trans>Sign in</Trans>}</Text>
-          </Button>
+          <form.Subscribe
+            children={({ isSubmitting }) => (
+              <Button
+                disabled={isSubmitting}
+                onPress={form.handleSubmit}
+                className="flex-row gap-2 self-stretch"
+              >
+                {isSubmitting && <ActivityIndicator />}
+                <Text>{isSubmitting ? <Trans>Signing in...</Trans> : <Trans>Sign in</Trans>}</Text>
+              </Button>
+            )}
+          />
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
