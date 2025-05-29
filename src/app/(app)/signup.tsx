e8bcd736a6ce70c9b55a0de8a9c7eb9d1fb3ec16
Allan Li <31210508+allanlidev@users.noreply.github.com>
2025-05-29 22:50:39 +0300

    refactor(login, signup): add form validation & submission state (#51)
    * refactor(login, signup): add form validation & submission state
    
    * chore(login): remove unused import
    
    ---------
    
    Co-authored-by: Emil Calonius <emil.calonius@gmail.com>

--- src/app/(app)/signup.tsx
+++ src/app/(app)/signup.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { ActivityIndicator, Alert, KeyboardAvoidingView, ScrollView, View } from 'react-native'
 import { Button } from '~/src/components/ui/button'
 import { Input } from '~/src/components/ui/input'
@@ -8,11 +7,28 @@ import { supabase } from '~/src/lib/supabase'
 import { Trans } from '@lingui/react/macro'
 import { t } from '@lingui/core/macro'
 import { H1 } from '~/src/components/ui/typography'
+import { z } from 'zod'
+import { passwordSchema } from '~/src/lib/schema'
+import { useForm } from '@tanstack/react-form'
+import { ErrorMessage } from '~/src/components/ErrorMessage'
 
 export default function Signup() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  const formSchema = z.object({
+    email: z.string().email({ message: t`Invalid email address` }),
+    password: passwordSchema,
+  })
+
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    validators: { onSubmit: formSchema },
+    onSubmit: async ({ value }) => {
+      const { email, password } = value
+      await signUpWithEmail({ email, password })
+    },
+  })
 
   /**
    * Sign up with email and password.
@@ -20,17 +36,12 @@ export default function Signup() {
    * It sets the loading state while the request is being processed,
    * and displays an alert with the result of the operation.
    */
-  async function signUpWithEmail() {
-    setIsLoading(true)
+  async function signUpWithEmail({ email, password }: { email: string; password: string }) {
     const {
       data: { session },
       error,
-    } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    })
+    } = await supabase.auth.signUp({ email, password })
     if (error) Alert.alert(error.message)
-    setIsLoading(false)
     if (session) {
       Alert.alert("You've successfully registered! Happy thrifting!")
     }
@@ -51,42 +62,72 @@ export default function Signup() {
             <Label nativeID="emailInput">
               <Trans>Email</Trans>
             </Label>
-            <Input
-              onChangeText={(text) => setEmail(text)}
-              value={email}
-              placeholder={t`email@address.com`}
-              autoCapitalize="none"
-              keyboardType="email-address"
-              autoComplete="email"
-              autoCorrect={false}
-              aria-labelledby="emailInput"
-              aria-errormessage="emailError"
+            <form.Field
+              name="email"
+              children={(field) => (
+                <>
+                  <Input
+                    onChangeText={field.handleChange}
+                    value={field.state.value}
+                    placeholder={t`email@address.com`}
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    autoComplete="email"
+                    autoCorrect={false}
+                    aria-labelledby="emailInput"
+                    aria-errormessage="emailError"
+                  />
+                  {field.state.meta.errors.length > 0 && (
+                    <ErrorMessage
+                      msg={field.state.meta.errors.map((error) => error?.message).join(', ')}
+                    />
+                  )}
+                </>
+              )}
             />
           </View>
           <View className="w-full gap-2">
             <Label nativeID="passwordInput">
               <Trans>Password</Trans>
             </Label>
-            <Input
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              placeholder={t`Password`}
-              autoCapitalize="none"
-              autoComplete="password"
-              autoCorrect={false}
-              aria-labelledby="passwordInput"
-              aria-errormessage="passwordError"
-              secureTextEntry
+            <form.Field
+              name="password"
+              children={(field) => (
+                <>
+                  <Input
+                    value={field.state.value}
+                    onChangeText={field.handleChange}
+                    placeholder={t`Password`}
+                    autoCapitalize="none"
+                    autoComplete="password"
+                    autoCorrect={false}
+                    aria-labelledby="passwordInput"
+                    aria-errormessage="passwordError"
+                    secureTextEntry
+                  />
+                  {field.state.meta.errors.length > 0 && (
+                    <ErrorMessage
+                      msg={field.state.meta.errors.map((error) => error?.message)[0] as string}
+                    />
+                  )}
+                </>
+              )}
             />
           </View>
-          <Button
-            disabled={isLoading}
-            onPress={() => signUpWithEmail()}
-            className="flex-row gap-2 self-stretch"
-          >
-            {isLoading && <ActivityIndicator />}
-            <Text>{isLoading ? <Trans>Registering...</Trans> : <Trans>Register</Trans>}</Text>
-          </Button>
+          <form.Subscribe
+            children={({ isSubmitting }) => (
+              <Button
+                disabled={isSubmitting}
+                onPress={form.handleSubmit}
+                className="flex-row gap-2 self-stretch"
+              >
+                {isSubmitting && <ActivityIndicator />}
+                <Text>
+                  {isSubmitting ? <Trans>Registering...</Trans> : <Trans>Register</Trans>}
+                </Text>
+              </Button>
+            )}
+          />
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
